ecc755c5077e6c514f551d34fccc6ec5156d372f
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-10-29 19:51:02 +0100

    Initial commit

---
+++ src/main/java/com/vijaygenius123/ServerManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerManagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/ServerManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
